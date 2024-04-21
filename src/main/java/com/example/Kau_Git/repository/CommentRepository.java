accdb590bf64a6b785f5d574c700419948f1a0d9
김종현 <sumei88@naver.com>
2024-04-22 01:29:57 +0900

    feat: 댓글달기, 개별 글에 달린 댓글목록, 댓글삭제

--- src/main/java/com/example/Kau_Git/repository/CommentRepository.java
+++ src/main/java/com/example/Kau_Git/repository/CommentRepository.java
@@ -1,10 +1,13 @@
 package com.example.Kau_Git.repository;
 
 import com.example.Kau_Git.entity.Comment;
+import com.example.Kau_Git.entity.Posting;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CommentRepository extends JpaRepository<Comment,Long> {
-    Comment findByCommentId(long commentId);
+    List<Comment> findAllByPostingOrderByCreatedAtAsc(Posting posting);
 }
