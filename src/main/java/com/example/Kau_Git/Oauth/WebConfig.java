ed1351c626eb86b0062806124f9c384056a9adae
bell2000 <thisisjhkim@naver.com>
2024-06-12 18:21:32 +0900

    fix: 프론트와의 연결문제, cors설정 추가

--- src/main/java/com/example/Kau_Git/Oauth/WebConfig.java
+++ src/main/java/com/example/Kau_Git/Oauth/WebConfig.java
@@ -2,14 +2,25 @@ package com.example.Kau_Git.Oauth;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http:parkingzone.shop") // 리액트 앱의 URL로 변경
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowCredentials(true); // 쿠키를 포함하여 요청을 허용
+    }
+
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new LoginUserArgumentResolver());
     }
+
 }
