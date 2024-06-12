3462ce8359c8366dfaa2f54fcd78eb7864a6ce70
bell2000 <thisisjhkim@naver.com>
2024-06-12 18:34:28 +0900

    fix: 프론트와의 연결문제, cors설정 추가3

--- src/main/java/com/example/Kau_Git/Oauth/WebConfig.java
+++ src/main/java/com/example/Kau_Git/Oauth/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000, http:parkingzone.shop") // 리액트 앱의 URL로 변경
+                .allowedOrigins("http://172.30.1.52:3000","http://localhost:3000, http:parkingzone.shop") // 리액트 앱의 URL로 변경
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true); // 쿠키를 포함하여 요청을 허용
     }
