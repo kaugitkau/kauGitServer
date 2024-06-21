c4add11d0813bf1c2888e5b9a33089f185d900f2
bell2000 <thisisjhkim@naver.com>
2024-06-21 14:34:13 +0900

    feat: 통일된 api 응답을 위한 ApiResponse 클래스 구현

--- src/main/java/com/example/Kau_Git/apiPayload/code/BaseCode.java
+++ src/main/java/com/example/Kau_Git/apiPayload/code/BaseCode.java
@@ -1,6 +1,6 @@
 package com.example.Kau_Git.apiPayload.code;
 
-public interface BaseCode {
+public interface BaseCode { //이 interface의 역할은, 이를 구체화 하는 Status에서 두 개의 메소드를 반드시 Override할 것을 강제하는 역할
 
     public ReasonDto getReason();
 
