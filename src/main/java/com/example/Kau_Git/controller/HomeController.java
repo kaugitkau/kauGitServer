a9b41bcc45469d1a934f1c71c2bf18bd36985386
qwer97 <mcrohmh@naver.com>
2024-06-24 20:38:56 +0900

    login변화

--- src/main/java/com/example/Kau_Git/controller/HomeController.java
+++ src/main/java/com/example/Kau_Git/controller/HomeController.java
@@ -63,13 +63,13 @@ public class HomeController {
 
         if ("naver".equals(provider)) {
             clientId = "ud72bzHYNtmwGhKFDjAU";
-            redirectUri = "http://parkingzone.shop/login/oauth2/code/naver";
+            redirectUri = "/login/oauth2/code/naver";
             authorizationUri = "https://nid.naver.com/oauth2.0/authorize";
             scopes.add("profile");
             scopes.add("email");
         } else if ("google".equals(provider)) {
             clientId = "8462816831-3lop9fop0m4utavosv1jns6t6g66qnt9.apps.googleusercontent.com";
-            redirectUri = "http://parkingzone.shop/login/oauth2/code/google";
+            redirectUri = "/login/oauth2/code/google";
             authorizationUri = "https://accounts.google.com/o/oauth2/auth";
             scopes.add("profile");
             scopes.add("email");
