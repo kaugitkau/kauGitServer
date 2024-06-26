28f105a01d7c6883037f1b1e18771670195aa6eb
bell2000 <thisisjhkim@naver.com>
2024-06-26 21:29:12 +0900

    update: 로그인 다시 업데이트

--- src/main/java/com/example/Kau_Git/controller/HomeController.java
+++ src/main/java/com/example/Kau_Git/controller/HomeController.java
@@ -39,57 +39,57 @@ public class HomeController {
         return "index";
     }
 
-    @GetMapping("/login")
-    @ResponseBody
-    public ResponseEntity<Map<String, String>> getLoginUrl(@RequestParam("provider") String provider) {
-        String loginUrl = "";
-        if ("naver".equals(provider)) {
-            loginUrl = "/oauth2/authorization/naver";
-        } else if ("google".equals(provider)) {
-            loginUrl = "/oauth2/authorization/google";
-        }
-        Map<String, String> response = new HashMap<>();
-        response.put("loginUrl", loginUrl);
-        return ResponseEntity.ok(response);
-    }
-
-
-//    @GetMapping("/login")//
-//    public RedirectView getLoginUrl(@RequestParam("provider") String provider, HttpServletResponse response) {
-//        String clientId = "";
-//        String redirectUri = "";
-//        String authorizationUri = "";
-//        Set<String> scopes = new HashSet<>();
-//
+//    @GetMapping("/login")
+//    @ResponseBody
+//    public ResponseEntity<Map<String, String>> getLoginUrl(@RequestParam("provider") String provider) {
+//        String loginUrl = "";
 //        if ("naver".equals(provider)) {
-//            clientId = "ud72bzHYNtmwGhKFDjAU";
-//            redirectUri = "/login/oauth2/code/naver";
-//            authorizationUri = "https://nid.naver.com/oauth2.0/authorize";
-//            scopes.add("profile");
-//            scopes.add("email");
+//            loginUrl = "/oauth2/authorization/naver";
 //        } else if ("google".equals(provider)) {
-//            clientId = "8462816831-3lop9fop0m4utavosv1jns6t6g66qnt9.apps.googleusercontent.com";
-//            redirectUri = "/login/oauth2/code/google";
-//            authorizationUri = "https://accounts.google.com/o/oauth2/auth";
-//            scopes.add("profile");
-//            scopes.add("email");
+//            loginUrl = "/oauth2/authorization/google";
 //        }
-//
-//        OAuth2AuthorizationRequest authorizationRequest = OAuth2AuthorizationRequestCreator.createAuthorizationRequest(clientId, redirectUri, authorizationUri, scopes);
-//        CookieUtils.addCookie(response, "oauth2_auth_request", CookieUtils.serialize(authorizationRequest), 180);
-//        String authorizationRequestUri = authorizationRequest.getAuthorizationRequestUri();
-//        return new RedirectView(authorizationRequestUri);
+//        Map<String, String> response = new HashMap<>();
+//        response.put("loginUrl", loginUrl);
+//        return ResponseEntity.ok(response);
 //    }
-//    @GetMapping("/login/oauth2/code/{provider}")
-//    public String handleOAuth2Callback(@PathVariable("provider") String provider, @RequestParam("code") String code, @RequestParam("state") String state, HttpServletRequest request, HttpServletResponse response) {
-//        Optional<Cookie> cookieOptional = CookieUtils.getCookie(request, "oauth2_auth_request");
-//        if (cookieOptional.isPresent()) {
-//            OAuth2AuthorizationRequest authorizationRequest = CookieUtils.deserialize(cookieOptional.get(), OAuth2AuthorizationRequest.class);
-//            // 인증 요청을 처리하고 사용자 정보를 가져오는 로직
-//            CookieUtils.deleteCookie(request, response, "oauth2_auth_request");
-//        }
-//        return "redirect:/";
-//    }
+
+
+    @GetMapping("/login")//
+    public RedirectView getLoginUrl(@RequestParam("provider") String provider, HttpServletResponse response) {
+        String clientId = "";
+        String redirectUri = "";
+        String authorizationUri = "";
+        Set<String> scopes = new HashSet<>();
+
+        if ("naver".equals(provider)) {
+            clientId = "ud72bzHYNtmwGhKFDjAU";
+            redirectUri = "/login/oauth2/code/naver";
+            authorizationUri = "https://nid.naver.com/oauth2.0/authorize";
+            scopes.add("profile");
+            scopes.add("email");
+        } else if ("google".equals(provider)) {
+            clientId = "8462816831-3lop9fop0m4utavosv1jns6t6g66qnt9.apps.googleusercontent.com";
+            redirectUri = "/login/oauth2/code/google";
+            authorizationUri = "https://accounts.google.com/o/oauth2/auth";
+            scopes.add("profile");
+            scopes.add("email");
+        }
+
+        OAuth2AuthorizationRequest authorizationRequest = OAuth2AuthorizationRequestCreator.createAuthorizationRequest(clientId, redirectUri, authorizationUri, scopes);
+        CookieUtils.addCookie(response, "oauth2_auth_request", CookieUtils.serialize(authorizationRequest), 180);
+        String authorizationRequestUri = authorizationRequest.getAuthorizationRequestUri();
+        return new RedirectView(authorizationRequestUri);
+    }
+    @GetMapping("/login/oauth2/code/{provider}")
+    public String handleOAuth2Callback(@PathVariable("provider") String provider, @RequestParam("code") String code, @RequestParam("state") String state, HttpServletRequest request, HttpServletResponse response) {
+        Optional<Cookie> cookieOptional = CookieUtils.getCookie(request, "oauth2_auth_request");
+        if (cookieOptional.isPresent()) {
+            OAuth2AuthorizationRequest authorizationRequest = CookieUtils.deserialize(cookieOptional.get(), OAuth2AuthorizationRequest.class);
+            // 인증 요청을 처리하고 사용자 정보를 가져오는 로직
+            CookieUtils.deleteCookie(request, response, "oauth2_auth_request");
+        }
+        return "redirect:/";
+    }
 
     @GetMapping("/")
     public ResponseEntity<?> home(@Login SessionUser user) {
